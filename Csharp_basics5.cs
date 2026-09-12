3ee9c2e17ddf99829f62349a73b0cfa8b0eedad7
NadaFayez13 <naditafayez13@gmail.com>
2026-09-12 17:10:00 +0300

    question 2

--- Csharp_basics5.cs
+++ Csharp_basics5.cs
@@ -6,10 +6,10 @@ namespace Assignment04
     {
         static void Main(string[] args)
         {
-            #region Question 1
-            // 1-Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price (index 1).
-            double[] prices1 = { 25.5, 40.0, 33.75 };
-            Console.WriteLine(prices1[1]);
+            #region Question 2
+            // 2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
+            int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            Console.WriteLine(shelfCopies[1, 0]);
             #endregion
         }
     }
