b8424ffcc762c07e9693f55d9e9b8e5011bacdb5
NadaFayez13 <naditafayez13@gmail.com>
2026-09-12 17:22:17 +0300

    question 3

--- Csharp_basics5.cs
+++ Csharp_basics5.cs
@@ -12,11 +12,25 @@ namespace Assignment04
            // Console.WriteLine(prices1[1]);
            // #endregion
 
-            #region Question 2
+           // #region Question 2
             // 2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
-            int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
-            Console.WriteLine(shelfCopies[1, 0]);
+           // int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+           // Console.WriteLine(shelfCopies[1, 0]);
+           // #endregion
+
+           #region Question 3
+            // 3-Write a method called PrintWelcomeMessage that takes no parameters and prints "Welcome to the Library!". Call it from Main.
+            PrintWelcomeMessage();
             #endregion
+
         }
+        
+        static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Library!");
+        }
+
+
+
     }
 }
