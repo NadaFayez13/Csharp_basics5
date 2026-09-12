38866da32f41ee012519bda47fff82fd7f2d6ae4
Aya Youssef <aya359602@gmail.com>
2026-09-12 16:55:17 +0300

    question 1

---
+++ Csharp_basics5.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Assignment04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            // 1-Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price (index 1).
+            double[] prices1 = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices1[1]);
+            #endregion
+        }
+    }
+}
